c496e55f52738b90fe8696a7926bdfb50c15e3ef
GilbertoCNetto <gilbertocurynetto@hotmail.com>
2024-10-04 12:02:12 -0300

    removendo pasta duplicada

--- Sprint 04/Desafio/mascarar/hash.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-
-while True:
-    string = input('Digite uma frase: ')
-
-    # Gerando o hash SHA-1
-    hash_sha1 = hashlib.sha1(string.encode()).hexdigest()
-    print(hash_sha1)
-    
