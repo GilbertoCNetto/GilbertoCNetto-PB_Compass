3a3856f072d3ffd661e534e1f1707df7357a15b1
GilbertoCNetto <gilbertocurynetto@hotmail.com>
2024-11-04 13:48:07 -0300

    alteração do codigo desafio.py

--- Sprint 06/Desafio/desafio.py
+++ Sprint 06/Desafio/desafio.py
@@ -7,9 +7,9 @@ nome_bucket = 'data-lake-do-gilberto'
 
 cliente_s3.create_bucket(Bucket=nome_bucket)
 
-caminho_arquivo_1 = r"/app/series.csv"
+caminho_arquivo_1 = r"/app/VOLUME/series.csv"
 
-caminho_arquivo_2 = r"/app/movies.csv"
+caminho_arquivo_2 = r"/app/VOLUME/movies.csv"
 
 nome_do_arquivo_1 = "series.csv"
 
